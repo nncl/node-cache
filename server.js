3f1f4ce37ca13dc6ab884e2f288a70dd9b27c226
caue <nncl@live.com>
2018-02-08 18:20:47 -0200

    Update

--- server.js
+++ server.js
@@ -2,11 +2,31 @@
 
 const express = require('express')
     , app = express()
-    , path = require('path');
+    , path = require('path')
+    , mcache = require('memory-cache')
+    , oneDay = 86400000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+let cache = (duration) => {
+    return (req, res, next) => {
+        let key = '__express__' + req.originalUrl || req.url;
+        let cachedBody = mcache.get(key);
+        if (cachedBody) {
+            res.send(cachedBody);
+            return;
+        } else {
+            res.sendResponse = res.send;
+            res.send = (body) => {
+                mcache.put(key, body, duration * 1000);
+                res.sendResponse(body)
+            };
+            next()
+        }
+    }
+};
 
-app.get('/', function (req, res) {
+app.use(express.static(path.join(__dirname, 'public'), {maxAge: oneDay}));
+
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
